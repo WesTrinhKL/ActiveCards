1c2e6f99d4a3a30e784f84890999dbd904e37ba3
wes <trinhpt@uci.edu>
2021-08-25 15:11:59 -0700

    added a wrapper to proect private quizzes

--- app/api/quiz_routes.py
+++ app/api/quiz_routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify
+import json
 from flask_login import login_required
 from app.models import User, QuizTemplate
 
@@ -7,13 +8,41 @@ quizzes_routes = Blueprint('quizzes', __name__)
 # ---------------Helpers-----------------
 
 
+def protect_private_quiz(template_func):
+    # decorator warpper method to only get public
+    def private_quiz_wrapper():
+        all_templates = template_func()
+        key = ''
+        if 'quiz_template_only' in all_templates:
+            key = 'quiz_template_only'
+        elif 'quizzes' in all_templates:
+            key = 'quizzes'
+        all_templates = all_templates[key]
+        return {key: [private_template for private_template in all_templates if private_template['is_private'] is False]}
+    return private_quiz_wrapper
 # ---------------Queries-----------------
-@quizzes_routes.route('/', methods=['GET'])
+
+
+@ quizzes_routes.route('/templates', methods=['GET'])
+@ protect_private_quiz
+def get_all_templates():
+    all_templates = QuizTemplate.query.limit(30).all()
+    return {'quiz_template_only': [template.to_dict() for template in all_templates]}
+
+
+@ quizzes_routes.route('/', methods=['GET'])
+# create wrapper to check if the quiz template is private or public
 def get_all_quizzes():
     all_quizzes = QuizTemplate.query.limit(30).all()
-    return {'quizzes': [quiz.to_dict() for quiz in all_quizzes]}
+    return {'quizzes': [quizzes.get_quiz_cards_with_all_relationship() for quizzes in all_quizzes]}
 
 
+@ quizzes_routes.route('/page/<int:page>', methods=['GET'])
+def get_paginated_quizzes(page):
+    per_page = 30
+    paged_quizzes = QuizTemplate.query.paginate(
+        page, per_page, error_out=False)
+    return {'quizzes': [quizzes.get_quiz_cards_with_all_relationship() for quizzes in paged_quizzes]}
 # ---------------QuizTemplate CRUD Routes-----------------
 
 
--- app/models/quiz_template.py
+++ app/models/quiz_template.py
@@ -7,6 +7,7 @@ class QuizTemplate(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
     description = db.Column(db.String(1000))
+    is_private = db.Column(db.Boolean, nullable=False)
 
     user_id = db.Column(db.Integer, db.ForeignKey(
         'users.id'), nullable=False)
@@ -26,6 +27,7 @@ class QuizTemplate(db.Model):
             'id': self.id,
             'title': self.title,
             'description': self.description,
+            'is_private': self.is_private,
             'user_relation': self.user_relation.to_dict(),
         }
 
@@ -33,6 +35,7 @@ class QuizTemplate(db.Model):
         return {
             'id': self.id,
             'title': self.title,
+            'is_private': self.is_private,
             'quiz_card_relation': sorted([card.to_dict() for card in self.quiz_card_relation], key=lambda i: i['id']),
             # example of sorting a diction by the key of age ---> sorted(lis, key = lambda i: i['age'])
             # 'user_active_recall_answer_relation': sorted([utility.to_dict() for utility in self.user_active_recall_answer_relation], key=lambda i: i['steps']),
