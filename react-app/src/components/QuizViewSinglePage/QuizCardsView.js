eefbfbe3d0e903b81e786f1cbe7b0594768dad1a
wes <trinhpt@uci.edu>
2021-08-30 13:08:37 -0700

    container css and content fit update on modals

--- react-app/src/components/QuizViewSinglePage/QuizCardsView.js
+++ react-app/src/components/QuizViewSinglePage/QuizCardsView.js
@@ -15,14 +15,17 @@ const QuizCardsView = () => {
       <div className="single-card-container">
         <div className="scs__buttons-container">
           {/* for each tab, add dynamic button and setTab to a property 'name-of-tab' */}
-          <button onClick={()=>setTab('active-recall')}>Active Recall</button>
-          <button onClick={()=>setTab('answers')}>Answers</button>
+          <div className={`${tab==='active-recall'? 'scs-bc__tab-button--active': ""} scs-bc__tab-button`} onClick={()=>setTab('active-recall')}>Active Recall</div>
+          <div className={`${tab==='answers'? 'scs-bc__tab-button--active': ""} scs-bc__tab-button`} onClick={()=>setTab('answers')}>Answers</div>
         </div>
         <div className="scs__content-container">
           {/* remember that each tab will have its own extensions. */}
           {tab==='active-recall' && <div>
             {/* in each tab, we'll have a button to add extension. Once clicked, you can select an extension from the collapsible on the right. Adding an extension will then add the component for that extension in the list that will be mapped. for each extension in extensions, render template and pass in extension data (or make individual extension components*/}
             active-recall content
+            <div> question</div>
+            <div> text area</div>
+            <div> answer history + save answer </div>
 
           </div>}
           {tab==='answers' && <div>
@@ -32,7 +35,9 @@ const QuizCardsView = () => {
         </div>
       </div>
 
-      <div className="add-card-container"> Add Card</div>
+      <div className="add-card-container">
+        <button>add card</button>
+      </div>
 
     </>
   )
