4d303835b21d6f8ab454b39c39c4b15f6d78c1a1
wes <trinhpt@uci.edu>
2021-09-14 23:49:01 -0700

    react test directory

--- react-app/src/components/WorkspacePage/Workspace.js
+++ react-app/src/components/WorkspacePage/Workspace.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './Workspace.css'
 const Workspace = () => {
   // use single fetch call to:
     // get all users workspace
     // get all users directory for each workspace
     // get all deck cover templates for each workspace
-
+  let workspaceAndDirectories = [
+    {home:
+      [
+        {first: ['deck1', 'deck2']}
+      ]
+    },
+    {interviews:
+      [
+        {javascript: ['deck1', 'deck2']},
+        {python: ['deck1', 'deck2']},
+        {software: ['deck1', 'deck2']},
+      ]
+    },
+    {art:
+      [
+        {french: ['deck1', 'deck2']},
+        {american: ['deck1', 'deck2, deck3']}
+      ]
+    }, ]
+    console.log("example of workspace", workspaceAndDirectories)
 
   return (
     <>
@@ -17,11 +36,16 @@ const Workspace = () => {
         <div className="wc__content-container">
           {/* Content Container changes depending on selected */}
           <div className="wc-cc__header">
-            content header
+            <div className="cc-header__bread-crumbs">content header / breadcrumbs...</div>
+
           </div>
           <div className="wc-cc__files-container">
             <div className="wc-cc-fc__content">
-              content folders or files
+              <div className="wc-cc-fc-content__wrapper">
+                <div className="content_wrapper__header">
+                  Interviews
+                </div>
+              </div>
             </div>
             <div className="wc-cc-fc__metadata">
               metadata
