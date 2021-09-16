c0120172510ca22f68796b3a4ee5d1cf03bf9748
wes <trinhpt@uci.edu>
2021-09-15 22:18:00 -0700

    react switching directories view

--- react-app/src/components/WorkspacePage/Workspace.js
+++ react-app/src/components/WorkspacePage/Workspace.js
@@ -3,40 +3,57 @@ import { useDispatch, useSelector } from 'react-redux'
 import './Workspace.css'
 import { getAllWorkspaceThunk, getAllUsersDecksDefaultDirectoryThunk } from '../../store/workspace_directories'
 const Workspace = () => {
-  // use single fetch call to:
-    // get all users workspace
-    // get all users directory for each workspace
-    // get all deck cover templates for each workspace
+  const dispatch = useDispatch();
 
   const currentUser = useSelector((state)=>state.session.user?.id)
-  // const allWorkspace = useSelector((state)=>)
-  const dispatch = useDispatch();
+  const allUserWorkspaces = useSelector((state)=>state.workspace_directories.all_workspace_and_children?.user_workspaces)
+  const defaultDirectory = useSelector((state)=>state.workspace_directories.default_deck?.all_user_decks)
 
   useEffect(() => {
     if(currentUser){
       dispatch(getAllWorkspaceThunk())
       dispatch(getAllUsersDecksDefaultDirectoryThunk())
     }
-
   }, [dispatch])
 
-  console.log("example of workspace")
+  // console.log("example", defaultDirectory)
 
-  const [workspaceViewOn, setworkspaceViewOn] = useState(true)
-  const [workspaceSelectedName, setworkspaceSelectedName] = useState('default')
+  // const [workspaceViewOn, setworkspaceViewOn] = useState(true)
+  // const [workspaceSelectedName, setworkspaceSelectedName] = useState('default')
+  const [directoriesViewOn, setdirectoriesViewOn] = useState(true)
+  const [directoriesSelectedId, setdirectoriesSelectedId] = useState('default') //will be default, until a directory is clicked on, then set Id to that.
 
+  const selectDirectoryE = (directory_id)=> {
+    setdirectoriesSelectedId(directory_id)
+  }
 
 
   return (
     <>
       <div className="workspace-container">
         <div className="wc__directories-nav">
-          Navigation / Directories
+          {/* <div className="directories-nav__title">Navigation / Directories</div> */}
+          <div className="directories-nav__workspace-directories">
+            {/* default */}
+            <div onClick={()=>setdirectoriesSelectedId('default')} className={directoriesSelectedId === 'default'?'workspace-directories__default workspace-selected':  'workspace-directories__default'}> default</div>
+
+            {/* map workspace and directories*/}
+            {allUserWorkspaces && (
+              allUserWorkspaces.map((workspace)=>{
+                return (
+                  <>
+                    <div className="workspace-nav">{workspace.name}</div>
+                    {workspace.directories.map(directory=> <div onClick={()=>selectDirectoryE(directory.id)} className={directoriesSelectedId === directory.id?'directory-nav directory-selected':  'directory-nav'}>{directory.name}</div> )}
+                  </>)
+              })
+            )}
+
+          </div>
+
           {/* onClick, set directoryView true and setDirectory to selected  */}
           {/* {workspaceAndDirectories.map(workspace => )} */}
           <div>
           </div>
-
         </div>
 
 
@@ -49,9 +66,9 @@ const Workspace = () => {
             <div className="wc-cc-fc__content">
               <div className="wc-cc-fc-content__wrapper">
                 <div className="content_wrapper__header">
-                  {workspaceViewOn && <div className="content__workspace-component"> {workspaceSelectedName} </div>}
+                  {directoriesViewOn && <div className="content__workspace-component"> {directoriesSelectedId} </div>}
 
-                  <div className="content__directory-component"> directory </div>
+                  <div className="content__directory-component"></div>
                 </div>
               </div>
             </div>
